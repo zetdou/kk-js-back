0a43928f42b829a0fc947c324ad127f42001c183
zetdou <zyziupoz@gmail.com>
2025-04-07 20:44:27 +0200

    Update productSchema
    Update schema of product to possible more advanced database

--- schemas/productSchema.js
+++ schemas/productSchema.js
@@ -2,14 +2,25 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
   {
-    id: String,
-    name: String,
-    description: String,
-    price: Number,
-    variant: String,
-    quantity: Number,
-    image: String,
-    category: String,
+    id: { type: String, required: true },
+    name: { type: String, required: true, trim: true },
+    description: { type: String, required: true },
+    variants: [
+      {
+        size: { type: String, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
+    reviews: [
+      {
+        userId: { type: String, required: true },
+        rating: { type: Number, required: true, min: 1, max: 5 },
+        comment: { type: String, required: true },
+      },
+    ],
+    quantity: { type: Number },
+    image: { type: String },
+    category: { type: String },
   },
   { versionKey: false }
 );
