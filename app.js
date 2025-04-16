cc25145cb7b5604cf53c5c9feb3c05e7e051fc49
zetdou <zyziupoz@gmail.com>
2025-04-16 19:31:26 +0200

    app.js
    Image of our products set to static - loaded from backend now

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const productsRouter = require("./routes/products");
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/products", productsRouter);
 
 app.use((req, res) => {
